ca0204b228915497bed98b60dc26f4aa170a7061
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2021-01-24 18:53:15 +0300

    init: start to implement logic

---
+++ src/components/Render/components/Content/GameListPage/app.js
@@ -0,0 +1,14 @@
+import Mixin from '@helpers/Mixin';
+
+const templateGameBlock = require('./assets/templates/game-block.html');
+const templateGameEnd = require('./assets/templates/recomended-game-block.html');
+const gameListPage = require('../../Content/assets/templates/game-list.html')
+
+// export default class GameListPage {
+//   constructor() {
+
+//   }
+// }
+
+console.log(gameListPage)
+
