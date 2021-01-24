ab455d3d4715b813eedd4ee7187326f15ccf8bfa
Adelheid483 <adelheid0106@gmail.com>
2021-01-24 23:36:51 +0300

    init: start work with burger menu

--- src/components/Render/components/Header/Header.js
+++ src/components/Render/components/Header/Header.js
@@ -11,6 +11,8 @@ export default class Header {
     this.node = this.getNode();
     this.elements = {
       logo: this.node.querySelector('.logo'),
+      burger: this.node.querySelector('#burger'),
+      overlay: this.node.querySelector('.burger__overlay'),
     };
 
     this.setHeaderListeners();
@@ -27,7 +29,37 @@ export default class Header {
     return node.firstChild;
   }
 
+  burgerBtnMethod() {
+    const burgerMenu = document.querySelector('.burger__menu');
+    burgerMenu.classList.toggle('active');
+    this.elements.burger.classList.toggle('active');
+    this.elements.overlay.classList.toggle('active');
+    if (burgerMenu.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  overlayMethod() {
+    const burgerMenu = document.querySelector('.burger__menu.active');
+    burgerMenu.classList.toggle('active');
+    this.elements.burger.classList.toggle('active');
+    this.elements.overlay.classList.toggle('active');
+    document.body.style.overflow = 'auto';
+  }
+
+  closeBurgerMenu() {
+    this.elements.overlay.classList.toggle('burger_active');
+    const burgerMenu = document.querySelector('.burger__menu.active');
+    burgerMenu.classList.toggle('active');
+    this.elements.burger.classList.toggle('active');
+    document.body.style.overflow = 'auto';
+  }
+
   setHeaderListeners() {
     this.elements.logo.addEventListener('click', () => console.log('logo clicked'));
+    this.elements.burger.addEventListener('click', () => this.burgerBtnMethod());
+    this.elements.overlay.addEventListener('click', () => this.overlayMethod());
   }
 }
