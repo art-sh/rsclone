4ae0cb7de6ade16493dba86c88e0e5a93126c3e5
Adelheid483 <adelheid0106@gmail.com>
2021-01-28 21:03:24 +0300

    feat: work with loader

--- src/components/Render/components/Loader321/Loader321.js
+++ src/components/Render/components/Loader321/Loader321.js
@@ -6,19 +6,32 @@ const template = require('./assets/template.html');
 export default class Loader321 {
   constructor(app) {
     this.$app = app;
+    this.template = template;
 
     this.elements = {
       body: this.node.querySelector('body'),
     };
 
+    Mixin.listen(this.$app.config.events.routeChange, this.destroyLoader.bind(this));
+  }
+
+  getLoaderContainer() {
+    const node = Mixin.getNode(this.template);
     this.setLoaderListeners();
+    return node;
   }
 
   showLoader() {
     setTimeout(() => {
+      this.getLoaderContainer();
       const loader = document.getElementById('loader321');
       if (!loader.classList.contains('load__done')) loader.classList.add('load__done');
     }, 3000);
+  }
+
+  destroyLoader() {
+    if (this.elements.node) this.elements.node.remove();
+    this.elements = {};
   }
 
   setLoaderListeners() {
