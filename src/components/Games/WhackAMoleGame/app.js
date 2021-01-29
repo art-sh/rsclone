368da97e217e13fce22b415d388d363d0b153743
Dmitry Shevchenok <dm.shevchenok@gmail.com>
2021-01-29 19:27:14 +0300

    fix: rework behavior of score adding and timer at whakAmole

--- src/components/Games/WhackAMoleGame/app.js
+++ src/components/Games/WhackAMoleGame/app.js
@@ -17,13 +17,16 @@ export default class WhackAMole {
     this.totalScore = 0;
     this.sessionScore = 0;
     this.isScoreCheat = false; // against repeated clicks on the mole
-    this.timeUp = false; // to the end of the game
     this.timer = new ReverseTimer();
     this.sessionTime = 10;
     this.timetoMs = 1000;
     this.minTime = 900;
     this.maxTime = 1000;
     this.stopGame = null;
+    this.scoreStep = 100;
+    this.scoreMultipliyer = 1;
+    this.isFirstStart = true;
+    this.moleCount = 0;
   }
 
   getGameNode() {
@@ -51,35 +54,45 @@ export default class WhackAMole {
   }
 
   startGame() {
+    if (!this.isFirstStart) {
+      this.resetFlagsHandler();
+      this.init();
+    }
+    this.isFirstStart = false;
+    this.finishBtnHandler('on');
     this.sessionScore = 0;
     this.isScoreCheat = false;
-    this.timeUp = false;
     this.showHideMoles(this.minTime, this.maxTime);
-    setTimeout(() => {
-      this.timeUp = true;
-      this.timer.startCount(this.sessionTime, this.setTimeText.bind(this));
-    }, this.sessionTime * this.timetoMs);
+    // setTimeout(() => {
+    // this.timeUp = true;
+    this.timer.startCount(60, this.setTimeText.bind(this), this.gameEnd.bind(this));
+    // }, this.sessionTime * this.timetoMs);
   }
 
   showHideMoles(from, to) {
+    this.moleCount += 1;
     const randomTime = this.randomTime(from, to);
     const randomHole = this.randomHole(this.holes);
     randomHole.classList.add('up');
     this.stopGame = setTimeout(() => {
       randomHole.classList.remove('up');
-      if (!this.timeUp) {
+      if (this.moleCount < 10) {
         this.showHideMoles(from, to);
         setTimeout(() => {
           this.isScoreCheat = false;
         }, randomTime / 5);
-      } else this.levelUp();
+      } else if (this.moleCount === 10 && this.sessionScore > 3) {
+        this.levelUp();
+      } else if (this.moleCount === 10 && this.sessionScore < 3) {
+        this.gameEnd();
+      }
     }, randomTime);
   }
 
   countScore(e) {
     if (!e.isTrusted) return; // protected from cheat
     if (!this.isScoreCheat) {
-      this.totalScore += 1;
+      this.totalScore += this.scoreStep * this.scoreMultipliyer;
       this.sessionScore += 1;
       this.moles.forEach((mole) => mole.classList.remove('up'));
       this.setScoreText(this.totalScore);
@@ -88,15 +101,18 @@ export default class WhackAMole {
   }
 
   levelUp() {
+    this.moleCount = 0;
+    this.scoreMultipliyer += 0.5;
     this.maxTime >= 100 ? this.maxTime -= 100 : this.maxTime = 100;
     this.minTime >= 100 ? this.minTime -= 100 : this.minTime = 100;
-    if (this.sessionScore >= 3) {
-      this.startGame();
-      this.$soundPlayer.playSound('level-next');
-    } else {
-      this.gameEnd();
-      this.$soundPlayer.playSound('level-down');
-    }
+    this.sessionScore = 0;
+    this.isScoreCheat = false;
+    this.showHideMoles(this.minTime, this.maxTime);
+    // setTimeout(() => {
+    //   this.timeUp = true;
+    //   this.timer.startCount(this.sessionTime, this.setTimeText.bind(this));
+    // }, this.sessionTime * this.timetoMs);
+    this.$soundPlayer.playSound('level-next');
   }
 
   randomTime(min, max) {
@@ -119,19 +135,41 @@ export default class WhackAMole {
     this.elements.stats.score.textContent = string.toString();
   }
 
-  disableFinishBtn() {
+  finishBtnHandler(mode = 'off') {
     this.elements.game.finishBtn.disabled = true;
     this.elements.game.finishBtn.classList.add('button_disabled');
     this.elements.game.finishBtn.style.cursor = 'default';
+    if (mode === 'on') {
+      this.elements.game.finishBtn.disabled = false;
+      this.elements.game.finishBtn.classList.remove('button_disabled');
+      this.elements.game.finishBtn.style.cursor = '';
+    }
+  }
+
+  resetFlagsHandler() {
+    clearTimeout(this.stopGame);
+    this.fieldSize = 9;
+    this.totalScore = 0;
+    this.sessionScore = 0;
+    this.isScoreCheat = false;
+    this.sessionTime = 10;
+    this.timetoMs = 1000;
+    this.minTime = 900;
+    this.maxTime = 1000;
+    this.stopGame = null;
+    this.timer.stopCount();
+    this.moleCount = 0;
+    this.scoreMultipliyer = 1;
   }
 
   showModalWindow() {
     const modal = new ModalWindow(this.$app);
     modal.showModal({
       type: this.gameConfig.modalWindow.types.gameEnd,
-      container: this.gameElement,
+      container: document.querySelector('#app'),
       text: {
         score: this.totalScore,
+        title: this.gameConfig.games.whackAMole.name,
       },
       callback: {
         restart: () => this.startGame(),
@@ -140,18 +178,20 @@ export default class WhackAMole {
   }
 
   destroyGameInstance() {
-    this.gameEnd();
+    this.timer.stopCount();
+    clearTimeout(this.stopGame);
     this.gameElement.remove();
   }
 
   gameEnd() {
-    this.disableFinishBtn();
+    this.finishBtnHandler();
+
     clearTimeout(this.stopGame);
     this.timer.stopCount();
     this.showModalWindow();
+
     this.gameElement.remove();
     this.isScoreCheat = false;
-    this.timeUp = false;
 
     return Mixin.dispatch(this.gameConfig.events.gameEnd, {
       game: this.gameConfig.id,
@@ -165,7 +205,6 @@ export default class WhackAMole {
     this.moles = document.querySelectorAll('.mole');
     this.moles.forEach((mole) => mole.addEventListener('click', this.countScore.bind(this)));
     this.setScoreText(0);
-    this.timer.startCount(this.sessionTime, this.setTimeText.bind(this));
   }
 
   init() {
