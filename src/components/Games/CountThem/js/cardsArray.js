dd811a58b16417e568bc71e02150b15dc178ecb9
Marinarium <schwmk@gmail.com>
2021-02-02 03:11:45 +0300

    feat: add the main elements and their styles

--- src/components/Games/CountThem/js/cardsArray.js
+++ src/components/Games/CountThem/js/cardsArray.js
@@ -3,52 +3,8 @@ import Mixin from '@helpers/Mixin';
 const imagesCollection = Mixin.handleWebpackImport(require.context('../assets/img', true, /\.png/));
 const cardsArray = [
   {
-    name: 'apple',
-    img: imagesCollection.apple,
-  },
-  {
-    name: 'ball',
-    img: imagesCollection.ball,
-  },
-  {
-    name: 'bee',
-    img: imagesCollection.bee,
-  },
-  {
-    name: 'compass',
-    img: imagesCollection.compass,
-  },
-  {
-    name: 'elephant',
-    img: imagesCollection.elephant,
-  },
-  {
-    name: 'four',
-    img: imagesCollection.four,
-  },
-  {
-    name: 'flower',
-    img: imagesCollection.flower,
-  },
-  {
-    name: 'leaf',
-    img: imagesCollection.leaf,
-  },
-  {
-    name: 'music',
-    img: imagesCollection.music,
-  },
-  {
-    name: 'sneakers',
-    img: imagesCollection.sneakers,
-  },
-  {
-    name: 'star',
-    img: imagesCollection.star,
-  },
-  {
-    name: 'turtle',
-    img: imagesCollection.turtle,
+    name: 'sheep',
+    img: imagesCollection.sheep,
   },
 ];
 
