adf22af934c2b678f5ee3ab266703d2362125786
Adelheid483 <adelheid0106@gmail.com>
2021-02-02 13:02:28 +0300

    fix: adjusting description of Matrix Memory

--- src/constant/config.js
+++ src/constant/config.js
@@ -20,7 +20,7 @@ export default {
     memoryMatrix: {
       name: 'Matrix Memory',
       id: 'matrix-memory',
-      description: 'The “Memory Matrix” simulator will make it easier to absorb new material, work with pronunciation and memorize grammar rules. What is more you will develop logical and analytical skills.',
+      description: 'The “Memory Matrix” simulator helps to develop the volume of short-term memory, accuracy and speed of memorization, spatial working memory, visual memory, voluntary attention, speed of thinking.',
       rules: 'Colored squares appear on the playing field. Your task will be to remember their location and accurately reproduce when they disappear. The time of appearance of the original value will decrease, and the field will increase. An incorrect answer results in a lower difficulty level and the loss of one of three lives.',
     },
     memoryGame: {
